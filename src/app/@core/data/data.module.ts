f1cd6f03bdeb0844e6bec632cfc7e52a5618bdbb
Asimple <946324894@qq.com>
2018-03-02 11:02:21 +0800

    app-model

---
+++ src/app/@core/data/data.module.ts
@@ -0,0 +1,33 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElectricityService } from './electricity.service';
+import { StateService } from './state.service';
+import { SmartTableService } from './smart-table.service';
+import { PlayerService } from './player.service';
+
+const SERVICES = [
+  ElectricityService,
+  StateService,
+  SmartTableService,
+  PlayerService,
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+  ],
+  providers: [
+    ...SERVICES,
+  ],
+})
+export class DataModule {
+  static forRoot(): ModuleWithProviders {
+    return <ModuleWithProviders>{
+      ngModule: DataModule,
+      providers: [
+        ...SERVICES,
+      ],
+    };
+  }
+}
